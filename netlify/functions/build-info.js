545cd42e4986206b9437c4380865e2cba12a1df8
oacoc3 <oacoc3@gmail.com>
2025-09-03 15:54:05 -0300

    Delete netlify/functions directory

--- netlify/functions/build-info.js
+++
@@ -1,7 +0,0 @@
-export default async () => {
-  const time = new Date().toISOString();
-  // Netlify disponibiliza COMMIT_REF e DEPLOY_ID
-  const commit = process.env.COMMIT_REF || '';
-  const deploy = process.env.DEPLOY_ID || '';
-  return new Response(JSON.stringify({ time, commit, deploy }), { headers: { 'Content-Type':'application/json' } });
-};
