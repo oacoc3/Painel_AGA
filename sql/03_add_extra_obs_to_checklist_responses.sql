33ec1851216550c9dc2bb4bf648496a1a3742979
oacoc3 <oacoc3@gmail.com>
2025-09-06 13:49:57 -0300

    Delete sql directory

--- sql/03_add_extra_obs_to_checklist_responses.sql
+++
@@ -1,4 +0,0 @@
--- sql/03_add_extra_obs_to_checklist_responses.sql
--- Adds the extra_obs column to store additional checklist comments
-alter table checklist_responses
-  add column if not exists extra_obs text;
