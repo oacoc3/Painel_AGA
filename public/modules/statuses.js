cbc9b895d9a77b487e63c4f7492abed0c67cb1d8
oacoc3 <oacoc3@gmail.com>
2025-09-17 19:35:33 -0300

    Update statuses.js

--- public/modules/statuses.js
+++ public/modules/statuses.js
@@ -3,7 +3,8 @@ window.Modules = window.Modules || {};
 window.Modules.statuses = (() => {
   const PROCESS_STATUSES = [
     'CONFEC','REV-OACO','APROV','ICA-PUB','EDICAO','AGD-LEIT','ANADOC',
-    'ANATEC-PRE','ANATEC','ANAICA','SOB-DOC','SOB-TEC','SOB-PDIR','SOB-EXPL','ARQ'
+    'ANATEC-PRE','ANATEC','ANAICA','SOB-DOC','SOB-TEC','SOB-PDIR','SOB-EXPL',
+    'DECEA','ARQ'
   ];
   return { PROCESS_STATUSES };
 })();
