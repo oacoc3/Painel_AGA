b11cb16d12cecc80d8d5bc042ab0593b28f93aef
oacoc3 <oacoc3@gmail.com>
2025-09-27 11:32:59 -0300

    Update config.js

--- public/config.js
+++ public/config.js
@@ -2,7 +2,7 @@
 // Preencha com as chaves do seu projeto. A anon key é pública por design no Supabase.
 window.APP_CONFIG = {
   SUPABASE_URL: "https://pshcwphtgwkwzosqgxrp.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9wb2dka3dhd2tsb3RtbmhiaHRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY5MjQzOTIsImV4cCI6MjA3MjUwMDM5Mn0.F-9WYgEKtF-YgQRH1tVJqgbs4HKSwHbKr2HCxCSYg1E",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBzaGN3cGh0Z3drd3pvc3FneHJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg5NjE0MDQsImV4cCI6MjA3NDUzNzQwNH0.MV4oQrRrJ32jZYI-vW7-ZQwg3rExNkDSLdEX7u2hgyo",
   NETLIFY_FUNCTIONS_BASE: "/.netlify/functions",
 
   // Versão manual do app (o rodapé mostrará "versão X").
